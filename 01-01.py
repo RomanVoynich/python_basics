d4289815f89f9b8d69ba9e2b1d239e356838cfcd
Roman Voynich <w99447@gmail.com>
2021-11-17 21:30:49 +0300

    changes

--- 01-01.py
+++ 01-01.py
@@ -1,2 +1,4 @@
-name = input('input your name:')
-print (name)
+name = input('input your name :')
+age = input("your age :")
+print ('Your name is ' + name + ' and you have ' + age + ' age')
+print ('Your name have '+str(len(name))+' letters')
