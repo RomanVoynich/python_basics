7f4eb864bb77b3cc710c08f276f6e993d5cbbaf9
Roman Voynich <w99447@gmail.com>
2021-11-19 14:57:22 +0300

    simple strings

--- 01-01.py
+++ 01-01.py
@@ -1,4 +1,11 @@
-name = input('input your name :')
-age = input("your age :")
-print ('Your name is ' + name + ' and you have ' + age + ' age')
-print ('Your name have '+str(len(name))+' letters')
+name = input('input your name : ')
+age = input("your age : ")
+
+# simple print
+#print ('Your name is ' + name + ' and you have ' + age + ' age')
+#print ('Your name have '+str(len(name))+' letters')
+
+# concatenation operator
+greet='yor name is '+name
+greet+=' and you have '+age+' age'
+print (greet)
