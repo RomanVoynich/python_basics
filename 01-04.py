8c8277ea87891a332f44c6cb8989aae3d52b1c9e
Roman Voynich <w99447@gmail.com>
2021-11-23 14:39:11 +0300

    operator if

---
+++ 01-04.py
@@ -0,0 +1,11 @@
+import random
+a = random.randrange(1, 10)
+b = random.randrange(1, 10)
+answer = input(f'{a}+{b}=')
+if answer.isdigit():
+    if int(answer) == a+b:
+        print('its ok')
+    else:
+        print('its not ok')
+else:
+    print('enter number')
